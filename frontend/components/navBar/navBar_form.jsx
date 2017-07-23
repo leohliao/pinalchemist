cded1cc90e30629ec02762faf4d9447bf35bd314
leol <ducleoliao@gmail.com>
2017-07-22 17:26:12 -0700

    Created NavBarForm class

---
+++ frontend/components/navBar/navBar_form.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class NavBarForm extends React.Component {
+  constructor(props){
+    super(props);
+
+  }//end constructor
+
+  render(){
+
+    return (
+      <div>
+        
+      </div>
+    );//end return
+  }//end render
+}//end NavBarform
