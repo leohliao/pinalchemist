a3a402daf9aac674bad31018f8e0d496fb1ef521
leol <ducleoliao@gmail.com>
2017-07-24 07:26:20 -0700

    Rewriting Pin Creat Form

--- frontend/components/pin/pinCreate/pin_create_form.jsx
+++ frontend/components/pin/pinCreate/pin_create_form.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import PinIndexItem from '../pinIndex/pin_index_item';
+import PinIndexForm from '../pinIndex/pin_index_form';
 import { selectAllPins } from '../../../reducers/selectors';
 import { values } from 'lodash';
 
@@ -9,43 +9,48 @@ class PinCreateForm extends React.Component {
     super(props);
 
     this.state = {
-      title: "",
-      description: "",
-      image_url: ""
+      pins: []
     };
 
     this.upload = this.upload.bind(this);
+    this.update = this.update.bind(this);
+    this.postImage = this.postImage.bind(this);
   }//end constructor
 
-  // getInititalState(){
-  //   return ({
-  //     pins: {}
-  //   });
-  // }//end getInititalState
-
   componentDidMount(){
-    $.get("/api/pins", pins => (
-      this.setState({ pins })
-    ));
+    $.get("/api/pins", images => (
+      this.setState({ pins: images })
+    ).bind(this));
   }//end componentDidMount
 
-  // postImage(uploadImage){
-  //   const data = {pin: { image_url: uploadImage.url}};
-  //   $.post("/api/pins", data, (savedImage) => {
-  //     let pins = this.props.pins;
-  //     pins.push(savedImage);
-  //     this.setState({ pins: pins });
-  //   });
-  // }//end postImage
+  postImage(image){
+    const data = { pins: { image_url: image.url} };
+    $.post("/api/pins", data, (savedImage) => {
+      let pins = this.state.pins;
+      pins.push(savedImage);
+      this.setState({ pins: pins });
+    });
+  }//end postImage
 
-  upload(event){
-    event.preventDefault();
-    cloudinary.openUploadWidget(cloudinary_options,(error, results) => {
+  upload(e){
+    e.preventDefault();
+    cloudinary.openUploadWidget(CLOUDINARY_OPTIONS, (error, results) => {
       if(!error){
         this.props.createPin(results[0]);
       }
     });
   }//end upload
+
+  update(property){
+    return e => this.setState({ [property]: e.target.value });
+  }//end update
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.state.upload(e);
+    this.props.createPin(this.state)
+      .then(data => this.props.history.push(`/posts/${data.id}`));
+  }//end handleSubmit
 
   render(){
     const { pins } = this.state;
@@ -58,11 +63,25 @@ class PinCreateForm extends React.Component {
       return null;
     }
 
+
     console.log(allPins);
     return(
       <div>
-        <button onClick={this.upload}>Upload new image!</button>
-
+        <form onSubmit={this.handleSubmit}>
+          <label>Title
+          <input onChange={this.update('title')}
+                 placeholder="title"
+                 value={this.state.title}/>
+          </label>
+          <label>Description
+          <input type="textarea"
+                 onChange={this.update('description')}
+                 placeholder="description"
+                 value={this.state.description}/>
+          </label>
+          <button className="create-button">Create</button>
+        </form>
+        <PinIndexForm pins={this.state.pins} />
       </div>
     );
   }//end render
