9ee5d68f4c5f467e6af5cf48745311a35c2d676d
leol <ducleoliao@gmail.com>
2017-08-30 11:14:51 -0700

    Added user_params in user_controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -28,6 +28,6 @@ class Api::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :image_url, :description)
   end
 end
