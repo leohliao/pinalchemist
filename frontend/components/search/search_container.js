5c88e24876506b1cd4b80de9ef2b18d95b08d528
leol <ducleoliao@gmail.com>
2017-10-12 14:26:35 -0700

    Added search container in component

---
+++ frontend/components/search/search_container.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import { requestSearch, resetSearch } from '../../actions/search_actions';
+import Search from './search';
+
+const mapStateToProps = ({ state }) => ({
+  currentUser: state.session.currentUser,
+  search: state.search
+});
+
+const mapDispatchToProps = dispatch => ({
+  requestSearch: search => dispatch(requestSearch(search)),
+  resetSearch: () => dispatch(resetSearch())
+});
+
+export default connect (
+  mapStateToProps,
+  mapDispatchToProps
+)(Search);
