1d6ef25be6c4a07ab62479e0fe1c144cce675ac4
leol <ducleoliao@gmail.com>
2017-10-12 14:17:38 -0700

    added search reducer

---
+++ frontend/reducers/search_reducer.js
@@ -0,0 +1,19 @@
+import * as searchActions from '../actions/search_actions';
+import merge from 'lodash/merge';
+
+const SearchReducer = (state={}, action) => {
+  Object.freeze(state);
+  switch(action.type) {
+    case searchActions.RECEIVE_SEARCH:
+      const search = action.search
+      return merge({}, state, { search });
+
+    case searchActions.RESET_SEARCH:
+      return {};
+
+    default:
+    return state;
+  }// end switch
+} // end SearchReducer
+
+export default SearchReducer;
