47ba375f15d3f73155e79dfb4e92a753a39e5990
leol <ducleoliao@gmail.com>
2017-11-25 01:18:00 -0800

    Search shows up

--- frontend/components/search/search.jsx
+++ frontend/components/search/search.jsx
@@ -29,41 +29,41 @@ class Search extends React.Component {
   // } // componentWillMount
 
   searchResults(items, type){
-    // let {resetSearch} = this.props;
-    // const resultsList = items.map((item, idx) => {
-    //       if (type === "Pins") {
-    //         return (
-    //           <li>
-    //             <PinShowContainer key={item.id} pin={item} />
-    //           </li>
-    //         );
-          // } else if ( type === "Boards") {
-          //   return (
-          //     <li key={idx} onClick={this.handleReset}>
-          //       <Link to={`/boards/${item.id}`}>
-          //         <span id="list-item">{item.title}</span>
-          //       </Link>
-          //   </li>
-          //   );
-          // } else if (type === "Users" ) {
-          //   return (
-          //     <li key={idx} onClick={this.handleReset}>
-          //       <Link to={`/users/${item.id}`}>
-          //         <img id="list-item-user"src={item.image_url}/>
-          //       <span id="list-item">{item.username}</span>
-          //       </Link>
-          //     </li>
-          //   );
-          // }
-    // }); // resultsList
+    let {resetSearch} = this.props;
+    const resultsList = items.map((item, idx) => {
+          if (type === "Pins") {
+            return (
+              <li key={idx} onClick={this.handleReset}>
+                <PinShowContainer key={item.id} pin={item} />
+              </li>
+            );
+          } else if ( type === "Boards") {
+            return (
+              <li key={idx} onClick={this.handleReset}>
+                <Link to={`/boards/${item.id}`}>
+                  <span id="list-item">{item.title}</span>
+                </Link>
+            </li>
+            );
+          } else if (type === "Users" ) {
+            return (
+              <li key={idx} onClick={this.handleReset}>
+                <Link to={`/users/${item.id}`}>
+                  <img id="list-item-user"src={item.image_url}/>
+                <span id="list-item">{item.username}</span>
+                </Link>
+              </li>
+            );
+          }
+    }); // resultsList
 
-    // return (
-    //   <div className={type === 'Users' ? "user-search-items-container" : "search-items-container"}>
-    //     <h3 className='search-type'>{type}</h3>
-    //     <ul className='section-items-container'>{listItems}</ul>
-    //     <br/>
-    //  </div>
-    // ) // return
+    return (
+      <div className={type === 'Users' ? "user-search-items-container" : "search-items-container"}>
+        <h3 className='search-type'>{type}</h3>
+        <ul className='section-items-container'>{resultsList}</ul>
+        <br/>
+     </div>
+    ) // return
   } // searchResults
 
   handleSearch(){
@@ -74,29 +74,29 @@ class Search extends React.Component {
     let pinsList;
     let boardsList;
     let usersList;
-    // if (Object.keys(search).length > 0) {
-    //   pinsList = pins.length > 0 ? this.searchResults(pins, "Pins") : null;
-    //   boardsList = boards.length > 0 ? this.searchResults(boards, "Boards") : null;
-    //   usersList = users.length > 0 ? this.searchResults(users, "Users") : null;
-    // } // end if
-    // let dropDown;
-    // document.addEventListener("click", (e)=> {
-    //   dropDown = document.getElementsByClassName('search-results-container')[0];
-    //   if (e.target.id === 'list-item' || e.target.id === 'list-item-user' || e.target.id === 'search-index-thumbnail') {
-    //     dropDown.style.display = 'none';
-    //   } else if ( e.path.includes(dropDown) || e.target.id === 'search-bar') {
-    //     dropDown.style.dispay = 'block';
-    //   } else {
-    //     dropDown.style.display = 'none';
-    //   } // end if
-    // }); // end document.addEventListener
+    if (Object.keys(search).length > 0) {
+      pinsList = pins.length > 0 ? this.searchResults(pins, "Pins") : null;
+      boardsList = boards.length > 0 ? this.searchResults(boards, "Boards") : null;
+      usersList = users.length > 0 ? this.searchResults(users, "Users") : null;
+    } // end if
+    let dropDown;
+    document.addEventListener("click", (e)=> {
+      dropDown = document.getElementsByClassName('search-results-container')[0];
+      console.log(dropDown);
+      // if (e.target.id === 'list-item' || e.target.id === 'list-item-user' || e.target.id === 'search-index-thumbnail') {
+      //   dropDown.style.display = 'none';
+      // } else if ( e.path.includes(dropDown) || e.target.id === 'search-bar') {
+      //   dropDown.style.dispay = 'block';
+      // } else {
+      //   dropDown.style.display = 'none';
+      // } // end if
+    }); // end document.addEventListener
 
     return (
       <div className='search-results-container'>
-        <h1>Hello</h1>
-        // {pinsList ? pinsList : null}
-        // {boardsList ? boardsList : null}
-        // {usersList ? usersList : null}
+        {pinsList ? pinsList : null}
+        {boardsList ? boardsList : null}
+        {usersList ? usersList : null}
       </div>
     ) // end return
   } // handleSearch
