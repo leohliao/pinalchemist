04c84594bebdba869a84321b7c60d6a1c1cc5e09
leol <ducleoliao@gmail.com>
2018-01-16 15:52:35 -0800

    Refactored Board_index

--- frontend/components/board/boardIndex/board_index.jsx
+++ frontend/components/board/boardIndex/board_index.jsx
@@ -3,22 +3,23 @@ import { withRouter } from 'react-router-dom';
 import Masonry from 'react-masonry-component';
 import { Link } from 'react-router-dom';
 import { values } from 'lodash';
+import { RingLoader } from 'react-spinners';
 import BoardItem from './board_item';
 import ModalBoardForm from '../boardForm/board_form_container';
 
 class BoardIndex extends React.Component {
   constructor(props) {
     super(props);
+    this.state= {
+      loading: true
+    }
 
   }//end constructor
 
-  // componentWillMount(){
-  //   this.props.requestUserBoards(this.props.currentUser.id);
-  // }
-  //
-  // componentDidMount(){
-  //   this.props.requestUserBoards(this.props.currentUser.id);
-  // }//end componentDidMount
+  componentDidMount(){
+    this.setState({ loading: false });
+  }//end componentDidMount
+
   handleCreateBoard(){
     return(
       <div className="modal-board-thumbnail-container">
@@ -47,51 +48,51 @@ class BoardIndex extends React.Component {
         };
 
     const allTheBoards = boards.map((board,idx) => (
-          // <BoardItem board={board} key={board.id} />
+        <Link to={`/boards/${board.id}`} key={idx} className="board-index-item-container">
             <div className="modal-board-thumbnail-container" key={idx}>
-              <div className="modal-board-thumbnail-container-title">{board.board_name}</div>
-              <Masonry className={'board-index-masonry'}
-                       elementType={'ul'}
-                       options={masonryOptionsThumnail}
-                       disableImagesLoaded={false}
-                       updateOnEachImageLoad={false}>
-                       { values(board.pins).slice(0,6).map( (pin,idx) => {
-                        return (
-                          <div key={idx} className='board-index-pin-thumbnails'>
-                            <img className='board-index-pin-img'src={pin.image_url}></img>
-                          </div>
-                        )
-                      })
-                    }
-              </Masonry>
-          </div>
+                <div className="modal-board-thumbnail-container-title">{board.board_name}</div>
+                <Masonry className={'board-index-masonry'}
+                         elementType={'ul'}
+                         options={masonryOptionsThumnail}
+                         disableImagesLoaded={false}
+                         updateOnEachImageLoad={false}>
+                         { values(board.pins).slice(0,8).map( (pin,idx) => {
+                          return (
+                            <div key={idx} className='board-index-pin-thumbnails'>
+                              <img className='board-index-pin-img'src={pin.image_url}></img>
+                            </div>
+                          )
+                        })
+                      }
+                </Masonry>
+            </div>
+        </Link>
     ));
 
-    return(
-      <div className="board-index-all">
-        <Masonry className={'board-index-masonry'}
-                 elementType={'ul'}
-                 options={masonryOptions}
-                 disableImagesLoaded={false}
-                 updateOnEachImageLoad={false}>
-            { this.handleCreateBoard() }
-            { allTheBoards }
-        </Masonry>
-      </div>
-    );//end return
+    if (this.state.loading){
+      return (
+        <div className='sweet-loading'>
+           <RingLoader
+             color={'#57bc90'}
+             loading={this.state.loading}
+           />
+        </div>
+      )
+    } else {
+      return(
+        <div className="board-index-all">
+          <Masonry className={'board-index-masonry'}
+                   elementType={'ul'}
+                   options={masonryOptions}
+                   disableImagesLoaded={false}
+                   updateOnEachImageLoad={false}>
+              { this.handleCreateBoard() }
+              { allTheBoards }
+          </Masonry>
+        </div>
+      );//end return
+    } // end if
   }//end render
 }//end BoardIndex
 
 export default BoardIndex;
-// <Masonry className={'board-index-masonry'}
-//          elementType={'ul'}
-//          options={masonryOptions}
-//          disableImagesLoaded={false}
-//          updateOnEachImageLoad={false}>
-//   <div className="modal-board-thumbnail-container">
-//     <ModalBoardForm currentUserImage={this.props.currentUser.image_url}/>
-//   </div>
-//   <div className="board-index-masonry-ul">
-//     { allTheBoards }
-//   </div>
-// </Masonry>
